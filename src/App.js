8f59c256fbd697af8ce38fa181a8af3c001799d5
amrbahaa <amrbahaa@amr.localdomain>
2022-11-15 01:19:31 +0200

    removed unimportant files and added comments for styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-// import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
-import * as ReactDOM from 'react-dom';
-import Card from './Card';
-import Cards from './Cards'
+// import logo from './logo.svg'; not deleted comment -1
+import "./App.css";
+import React, { Component } from "react";
+// import * as ReactDOM from 'react-dom';
+// import Card from './Card';
+// not used imports -3
+import Cards from "./Cards";
 
-
-
-class App extends Component{
-  
-
-  
-  render() {
-    return (
-      <div className="App">
-
-        <Cards />
-      
-       
-      </div>
-    );
-  }
+// don't leave any extra spaces -3 
+// always re-format your code
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Cards />
+			</div>
+		);
+	}
 }
-
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// not removed comments -3 
 // import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
@@ -17,7 +18,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
